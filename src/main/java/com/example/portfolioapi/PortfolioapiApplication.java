f12f644712cdfd4b91e048ba506c1766da25352f
María Victoria Gálvez <94771177+tutigalvez@users.noreply.github.com>
2022-05-14 00:49:25 -0300

    api

---
+++ src/main/java/com/example/portfolioapi/PortfolioapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.portfolioapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioapiApplication.class, args);
+	}
+
+}
