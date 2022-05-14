a35d18fc529d86862382f453a299443b7d8cd747
tutigalvez <tutigalvez92@gmail.com>
2022-05-14 15:37:37 -0300

    controller

--- src/main/java/com/example/portfolioapi/controllers/RedesController.java
+++ src/main/java/com/example/portfolioapi/controllers/RedesController.java
@@ -8,6 +8,7 @@ import com.example.portfolioapi.services.RedesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,7 +17,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin("*")
 @RestController
